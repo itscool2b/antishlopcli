f5b24e22782a1a7d63aa44a35d0ddce3c56882d7
itscool2b <megagrb16@gmail.com>
2025-08-21 19:31:56 -0400

    updated readme

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name='antishlop-cli',
-    version='1.0.1',
+    version='1.1.1',
     description='Anti-vibe coding and security agent',
     long_description=long_description,
     long_description_content_type='text/markdown',
